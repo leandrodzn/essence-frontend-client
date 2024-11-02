a5f4a29d06bc23052c5ac0c76e7d60e7f30beaeb
leandrodzn <leodzib100@gmail.com>
2024-11-02 17:45:05 -0600

    Comment out server configuration in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,10 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  server: {
-    host: true, // Escucha en todas las interfaces de red
-    port: 5173, // O cualquier puerto que prefieras
-  },
+  // server: {
+  //   host: true, // Escucha en todas las interfaces de red
+  //   port: 5173, // O cualquier puerto que prefieras
+  // },
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
