5f79cc4af5dfc8c4d5f6bf136a5195a463b7a855
leandrodzn <leodzib100@gmail.com>
2025-01-03 21:14:06 -0600

    Enable history API fallback in Vite configuration for vue-router support

--- vite.config.js
+++ vite.config.js
@@ -6,14 +6,13 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  // server: {
-  //   host: true, // Escucha en todas las interfaces de red
-  //   port: 5173, // O cualquier puerto que prefieras
-  // },
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
       "@axios": fileURLToPath(new URL("./src/config/axios", import.meta.url)),
     },
+  },
+  server: {
+    historyApiFallback: true, // for vue-router
   },
 });
